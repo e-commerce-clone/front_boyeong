9280d02a145e3c02b29481d73956e0086d230d3b
boyouth <qhdudqkra@gmail.com>
2021-02-20 21:55:50 +0900

    코드 수정

--- 2021-02-11/js/customer.js
+++ 2021-02-11/js/customer.js
@@ -16,3 +16,12 @@ function order_open() {
 function order_close() {
     $('#ifm_order').removeClass('add');
 }
+
+// $(document).ready(function(){
+//     $("input:radio[name=ordernoSelect]").click(function(){
+//         if ($("input[name=radio]:checked").val()=="1"){
+//             $("")
+//         }
+//     })
+// })
+// 부모 자식 창으로 해야됨. 라디오버튼 클릭 시 부모창에 값이 전달되어야 함.
